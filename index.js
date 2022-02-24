31e038fae4aa939f431d6ae56bf0ccc8400e5f0d
Alisa Gezalova <alisa.gezalova@gmail.com>
2022-02-23 22:43:30 -0700

    commit tree 663e1d589caf015b306963f6b63bc47915dbe1ea

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+
+const input = document.getElementById('input');
+ 
 function addingEventListener() {
+  alert('I was clicked!');
+  return input.addEventListener('click', addingEventListener)
 }
+ 
